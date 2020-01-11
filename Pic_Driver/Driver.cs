d30e437569c644e07953a2b21b9bc66ca204652b
metalsonic21 <45906155+metalsonic21@users.noreply.github.com>
2020-01-11 01:46:24 -0400

    Receives data from COM1

---
+++ Pic_Driver/Driver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Serial COM library */
+using System.IO.Ports;
+
+namespace Pic_Driver
+{
+    class Driver
+    {
+        // Create the serial port with basic settings 
+        SerialPort port = new SerialPort("COM1",
+          9600, Parity.None, 8, StopBits.One);
+        [STAThread]
+
+        private void SerialPortProgram()
+        {
+            Console.WriteLine("Datos enviados desde el PIC:");
+            port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+
+            // Open port
+            port.Open();
+            // Enter an application loop to keep this thread alive 
+            Console.ReadLine();
+        }
+
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // Show all the incoming data in the port's buffer
+            Console.WriteLine(port.ReadExisting());
+        }
+
+        static void Main(string[] args)
+        {
+            Driver d = new Driver();
+            d.SerialPortProgram();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Pic_Driver
+ Fetches data sent from serial COM and does stuff depending of the output
